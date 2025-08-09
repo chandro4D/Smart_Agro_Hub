bf90ffcd6d723611abe499676119fb60f32498a3
chandro4D <chandroshakhor7@gmail.com>
2025-08-09 15:01:27 +0600

    Add Db

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,42 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import cors from "cors";
+import productRoutes from "./routes/productRoutes.js"
+import { sql } from "./config/db.js";
+
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res) =>{
-    res.send("Hello From The Backend");
+app.use(express.json());
+app.use(helmet()); // Security middleware
+app.use(morgan("dev")); //Log the requests
+app.use(cors());
+
+app.use("/api/products", productRoutes);
+
+
+async function initDB() {
+    try {
+        await sql`
+          CREATE TABLE IF NOT EXISTS products (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            image VARCHAR(255) NOT NULL,
+            price DECIMAL(10,2) NOT NULL,
+            create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+          )
+        `;
+        console.log("DataBase Initialization successfully");
+    } catch (error) {
+        console.log("Error InitDB", error);
+    }
+}
+console.log();
+initDB().then(() => {
+    app.listen(3000, () => {
+        console.log(`Server is running on port ` + PORT);
+    });
 })
-
-
-app.listen(3000,() =>{
-    console.log("Server is running on port 3000");
-});
