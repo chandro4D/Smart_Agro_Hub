32402b61def4772b313ca95d5ac68b1a547dce75
chandro4D <chandroshakhor7@gmail.com>
2025-09-03 23:29:16 +0600

    add update profile backend

--- Backend/controllers/dashboardController.js
+++ Backend/controllers/dashboardController.js
@@ -9,3 +9,30 @@ export const sellerDashboard = (req, res) => {
 export const userDashboard = (req, res) => {
   res.json({ message: "Welcome User! This is your dashboard." });
 };
+
+//For Update product
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, price, image, category } = req.body;
+
+  try {
+    const updateProduct = await sql`
+      UPDATE products
+      SET name=${name}, price=${price}, image=${image}, category=${category}
+      WHERE id=${id}
+      RETURNING *
+    `;
+
+    if (updateProduct.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({ success: true, data: updateProduct[0] });
+  } catch (error) {
+    console.log("Error in updateProduct function", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
